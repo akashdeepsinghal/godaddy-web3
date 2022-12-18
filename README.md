7c194f12efbe2a018ec6bae0eaf7ab70b94c31ed
Akash Deep Singhal <akigupta131@gmail.com>
2022-12-18 11:25:02 +0530

    Solidity contract constructor setup and basic test cases

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # godaddy-web3
 Your web3 username, a name for all your cryptocurrency addresses, and decentralised websites.
+
+## Solidity Contract functions
+1. List domain
+2. Buy Domain
+3. Pay Fees
--- test/GodaddyWeb3.js
+++ test/GodaddyWeb3.js
@@ -1,7 +1,33 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
-describe("GodaddyWeb3", () => {});
+describe("GodaddyWeb3", () => {
+  let godaddyWeb3;
+  let deployer, owner1;
+  const NAME = "Akash Singhal";
+  const SYMBL = "ADS";
+  beforeEach(async () => {
+    [deployer, owner1] = await ethers.getSigners();
+    const GodaddyWeb3 = await ethers.getContractFactory("GodaddyWeb3");
+    godaddyWeb3 = await GodaddyWeb3.deploy(NAME, SYMBL);
+  });
+
+  describe("Deployment", async () => {
+    it("has a name", async () => {
+      const result = await godaddyWeb3.name();
+      expect(result).to.be.equal(NAME);
+    });
+    it("has a symbol", async () => {
+      const result = await godaddyWeb3.symbol();
+      expect(result).to.be.equal(SYMBL);
+    });
+    it("sets the owner", async () => {
+      const result = await godaddyWeb3.owner();
+      expect(result).to.be.equal(deployer.address);
+    });
+  });
+});
