ca1120a6c8098586a058205c8dd2f6828f0d1291
Akash Deep Singhal <akigupta131@gmail.com>
2022-12-19 12:21:07 +0530

    List domains sol function and test cases written

--- contracts/GodaddyWeb3.sol
+++ contracts/GodaddyWeb3.sol
@@ -4,11 +4,30 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract GodaddyWeb3 is ERC721 {
+    uint256 public maxSupply;
     address public owner;
+
+    struct Domain {
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+
+    mapping(uint256 => Domain) public domains;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only Owner can call this method");
+        _;
+    }
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {
         owner = msg.sender;
+    }
+
+    function list(string memory _name, uint256 _cost) public onlyOwner {
+        maxSupply++;
+        domains[maxSupply] = Domain(_name, _cost, false);
     }
 }
--- test/GodaddyWeb3.js
+++ test/GodaddyWeb3.js
@@ -8,26 +8,50 @@ const tokens = (n) => {
 describe("GodaddyWeb3", () => {
   let godaddyWeb3;
   let deployer, owner1;
-  const NAME = "Akash Singhal";
-  const SYMBL = "ADS";
+  const CONTRACT_NAME = "Akash Singhal";
+  const CONTRACT_SYMBOL = "ADS";
+  const DOMAIN_NAME = "akash.eth";
+  const DOMAIN_COST = tokens(10);
   beforeEach(async () => {
+    // Setup accounts
     [deployer, owner1] = await ethers.getSigners();
+
+    // Deploy contract
     const GodaddyWeb3 = await ethers.getContractFactory("GodaddyWeb3");
-    godaddyWeb3 = await GodaddyWeb3.deploy(NAME, SYMBL);
+    godaddyWeb3 = await GodaddyWeb3.deploy(CONTRACT_NAME, CONTRACT_SYMBOL);
+
+    // List property
+    let transaction = await godaddyWeb3
+      .connect(deployer)
+      .list(DOMAIN_NAME, tokens(10));
+    await transaction.wait();
   });
 
   describe("Deployment", async () => {
     it("has a name", async () => {
       const result = await godaddyWeb3.name();
-      expect(result).to.be.equal(NAME);
+      expect(result).to.be.equal(CONTRACT_NAME);
     });
     it("has a symbol", async () => {
       const result = await godaddyWeb3.symbol();
-      expect(result).to.be.equal(SYMBL);
+      expect(result).to.be.equal(CONTRACT_SYMBOL);
     });
     it("sets the owner", async () => {
       const result = await godaddyWeb3.owner();
       expect(result).to.be.equal(deployer.address);
+    });
+    it("Returns the maxSupply", async () => {
+      const result = await godaddyWeb3.maxSupply();
+      expect(result).to.be.equal(1);
+    });
+  });
+
+  describe("Listing", async () => {
+    it("Returns domain attributes", async () => {
+      const result = await godaddyWeb3.domains(1);
+      expect(result.name).to.be.equal(DOMAIN_NAME);
+      expect(result.cost).to.be.equal(DOMAIN_COST);
+      expect(result.isOwned).to.be.equal(false);
     });
   });
 });
